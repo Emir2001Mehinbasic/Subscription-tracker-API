b4b0f3fe3e512f8009dd72bec4d63ae42f82b02c
Emir Mehinbasic <emir96meh@gmail.com>
2025-08-28 11:21:03 +0200

    Added Controler for sign up function

---
+++ controllers/auth.controller.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const signUp = async (req, res,next) => {
+  //logic for user sign up
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    const { name , email, password } = req.body;
+
+    const existingUser = await  User.findOne({ email }).session(session);
+    if (existingUser) {
+      const error = new Error('Email already in use');
+      error.statusCode = 409;
+      throw error;
+    }
+    // Hash the password before saving
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = await User.create([{ name, email, password: hashedPassword }], { session });
+
+    const token = jwt.sign({ userId: newUser[0]._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+
+   await session.commitTransaction();
+   session.endSession();
+
+    res.status(201).json({ 
+      success: true,
+      data: {
+        user: {
+          id: newUser[0]._id,
+          name: newUser[0].name,
+          email: newUser[0].email,
+        },
+        token
+      }
+    });
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    next(error);
+  }
+}
+
+export const signIn = async (req, res,next) => {
+  //logic for user sign in
+
+}
+
+export const signOut = async (req, res,next) => { 
+    //logic for user sign out
+ }
