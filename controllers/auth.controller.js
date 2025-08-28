f807218cf052c53ff160a1b4555f270df9e4d9a8
Emir Mehinbasic <emir96meh@gmail.com>
2025-08-28 13:59:38 +0200

    Added authorization middleware

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -35,8 +35,8 @@ export const signUp = async (req, res,next) => {
           id: newUser[0]._id,
           name: newUser[0].name,
           email: newUser[0].email,
+          token
         },
-        token
       }
     });
   } catch (error) {
@@ -48,6 +48,39 @@ export const signUp = async (req, res,next) => {
 
 export const signIn = async (req, res,next) => {
   //logic for user sign in
+  try{
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      const error = new Error('User not found');
+      error.statusCode = 404;
+      throw error;
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      const error = new Error('Invalid password');
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+
+    return res.status(200).json({ 
+      success: true,
+      message : 'User signed in successfully',
+      data: {
+        user: {
+          id: user._id,
+          name: user.name,
+          email: user.email,
+          token
+        },
+      }
+    });
+  }catch(error){
+    next(error);
+  }
 
 }
 
