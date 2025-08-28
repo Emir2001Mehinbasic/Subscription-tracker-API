1cc202cf70373da2df833c9717a51e4849154f62
Emir Mehinbasic <emir96meh@gmail.com>
2025-08-28 10:20:41 +0200

    Connected database (mongodb) with mongoose ORM

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 
 import subscriptionRouter from './routes/subscription.routes.js';
+import { connect } from 'mongoose';
+import connectDB from './database/mongodb.js';
 
 const app = express();
  
@@ -15,12 +17,15 @@ app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
 
 
+
 app.get('/', (req, res) => {
     res.send('Welcome to the Subscription Tracker API');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
+
+   await connectDB();
 });
 
 export default app;
